82db43d24ccee9e5c79459c9d97b5a3d2b9c8bec
DV-00 <dipakvanzara@gmail.com>
2025-02-04 23:19:27 +0530

    Initial commit of ecommerce backend microservices

---
+++ ecommerce-website/productservice/src/main/java/com/ecommerce/productservice/models/SelfDb.java
@@ -0,0 +1,19 @@
+package com.ecommerce.productservice.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "custom_users")
+
+public class SelfDb {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String discription;
+
+
+
+}
