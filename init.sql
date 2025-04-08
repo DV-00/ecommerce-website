fa465497cf803bcd5955c7fbc637954173501cb7
DV-00 <dipakvanzara@gmail.com>
2025-04-08 09:40:43 +0530

    dockercompose.yml added

---
+++ init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS userservice_db;
+CREATE DATABASE IF NOT EXISTS productservice_db;
+CREATE DATABASE IF NOT EXISTS orderservice_db;
+CREATE DATABASE IF NOT EXISTS cartservice_db;
+CREATE DATABASE IF NOT EXISTS paymentservice_db;
